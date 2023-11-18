6b24d8dcc26a3e1a046d2929c17d21d07d710c35
Harsh Tyagi <tyagiharsh216@gmail.com>
2023-11-18 19:38:02 +0530

    parser and interpreter, along with adding a new SymbolTable class.

--- B1_500091612.py
+++ B1_500091612.py
@@ -357,6 +357,83 @@ class SymbolTable:
 
 	def remove(self, name):
 		del self.symbols[name]
+class Interpreter:
+	def visit(self, node, context):
+		method_name = f'visit_{type(node).__name__}'
+		method = getattr(self, method_name, self.no_visit_method)
+		return method(node, context)
+
+	def no_visit_method(self, node, context):
+		raise Exception(f'No visit_{type(node).__name__} method defined')
+
+	###################################
+
+	def visit_NumberNode(self, node, context):
+		return RTResult().success(
+			Number(node.tok.value).set_context(context).set_pos(node.pos_start, node.pos_end)
+		)
+
+	def visit_VarAccessNode(self, node, context):
+		res = RTResult()
+		var_name = node.var_name_tok.value
+		value = context.symbol_table.get(var_name)
+
+		if not value:
+			return res.failure(RTError(
+				node.pos_start, node.pos_end,
+				f"'{var_name}' is not defined",
+				context
+			))
+
+		value = value.copy().set_pos(node.pos_start, node.pos_end)
+		return res.success(value)
+
+	def visit_VarAssignNode(self, node, context):
+		res = RTResult()
+		var_name = node.var_name_tok.value
+		value = res.register(self.visit(node.value_node, context))
+		if res.error: return res
+
+		context.symbol_table.set(var_name, value)
+		return res.success(value)
+
+	def visit_BinOpNode(self, node, context):
+		res = RTResult()
+		left = res.register(self.visit(node.left_node, context))
+		if res.error: return res
+		right = res.register(self.visit(node.right_node, context))
+		if res.error: return res
+
+		if node.op_tok.type == TT_PLUS:
+			result, error = left.added_to(right)
+		elif node.op_tok.type == TT_MINUS:
+			result, error = left.subbed_by(right)
+		elif node.op_tok.type == TT_MUL:
+			result, error = left.multed_by(right)
+		elif node.op_tok.type == TT_DIV:
+			result, error = left.dived_by(right)
+		elif node.op_tok.type == TT_POW:
+			result, error = left.powed_by(right)
+
+		if error:
+			return res.failure(error)
+		else:
+			return res.success(result.set_pos(node.pos_start, node.pos_end))
+
+	def visit_UnaryOpNode(self, node, context):
+		res = RTResult()
+		number = res.register(self.visit(node.node, context))
+		if res.error: return res
+
+		error = None
+
+		if node.op_tok.type == TT_MINUS:
+			number, error = number.multed_by(Number(-1))
+
+		if error:
+			return res.failure(error)
+		else:
+			return res.success(number.set_pos(node.pos_start, node.pos_end))
 #######################################
 # LEXER
 #######################################
@@ -523,6 +600,29 @@ def run(fn, text):
 
     return tokens, error
 
+global_symbol_table = SymbolTable()
+global_symbol_table.set("NULL", Number(0))
+global_symbol_table.set("FALSE", Number(0))
+global_symbol_table.set("TRUE", Number(1))
+
+def run(fn, text):
+	# Generate tokens
+	lexer = Lexer(fn, text)
+	tokens, error = lexer.make_tokens()
+	if error: return None, error
+	
+	# Generate AST
+	parser = Parser(tokens)
+	ast = parser.parse()
+	if ast.error: return None, ast.error
+
+	# Run program
+	interpreter = Interpreter()
+	context = Context('<program>')
+	context.symbol_table = global_symbol_table
+	result = interpreter.visit(ast.node, context)
+
+	return result.value, result.error
 
 ###################### 
 #######  Lex program to count the number of lines, spaces and tabs #####
